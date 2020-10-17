d8a1b5216933f693ef9bdeffb91e295cc23b74e1
anamfazal <anamfazal94@gmail.com>
2020-10-17 23:36:31 +0530

    Refactored: password validation code to have at least 1 digit

--- UserRegistration.java
+++ UserRegistration.java
@@ -1,4 +1,4 @@
-/* Purpose: Validate password with at least 1 upper case */
+/* Purpose: Validate password with at least 1 number */
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -22,7 +22,7 @@ public class UserRegistration {
 	private void validateLastName() {
 		Scanner scan = new Scanner(System.in);
 		System.out.println(" Enter last name: ");
-		String lastName = scan.next();				 //take first name from user
+		String lastName = scan.next();				 //take last name from user
 		String regexLastName = "^[A-Z]{1}[a-z]{2,}$";		 //regex definition
 		Pattern pattern = Pattern.compile(regexLastName);
 		Matcher last_nameMatcher = pattern.matcher(lastName);
@@ -33,7 +33,7 @@ public class UserRegistration {
 	/* method to validate email */
 	private void validateEmail() {
 		Scanner scan = new Scanner(System.in);
-		System.out.println("Enter email: ");
+		System.out.println("Enter email: ");			//take email from user
 		String email = scan.next();
 		String regexEmail = "^[A-Za-z0-9]+([-\\.\\+\\_]{1}[0-9A-Za-z]+)*@[A-Za-z0-9]+.[a-zA-Z]{2,4}([\\.\\,]{1}[a-z]{2,3}){0,1}$";
 		Pattern pattern = Pattern.compile(regexEmail);
@@ -45,7 +45,7 @@ public class UserRegistration {
 	/* method to validate password */
 	private void validatePassword() {
 		Scanner scan = new Scanner(System.in);
-		System.out.println("Enter password with min 8 characters: ");
+		System.out.println("Enter password with min 8 characters,at least 1 uppercase letter and digit: ");
 		String password = scan.next();
 		String regexPassword1 = "^.{8,}";			//regex pattern for min 8 characters
 		Pattern pattern1 = Pattern.compile(regexPassword1);
@@ -53,7 +53,11 @@ public class UserRegistration {
 		String regexPassword2 = ".*[A-Z]{1,}.*";	        //regex pattern for min 1 uppercase
 		Pattern pattern2 = Pattern.compile(regexPassword2);
 		Matcher passwordMatcher2 = pattern2.matcher(password);
-		if (passwordMatcher1.matches() && passwordMatcher2.matches() == true) 
+		String regexPassword3 = ".*[0-9]{1,}.*";                //regex pattern for min 1 digit
+                Pattern pattern3 = Pattern.compile(regexPassword3);
+                Matcher passwordMatcher3 = pattern3.matcher(password);
+
+		if (passwordMatcher1.matches() && passwordMatcher2.matches() && passwordMatcher3.matches() == true) 
 			System.out.println("true");	
 		else 
 			System.out.println("false");
