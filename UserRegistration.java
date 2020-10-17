39790837840668741a9f4797cd5bbce9786c417c
anamfazal <anamfazal94@gmail.com>
2020-10-17 23:25:41 +0530

    Refactored: password validation code to include min 1 upper case

--- UserRegistration.java
+++ UserRegistration.java
@@ -1,4 +1,4 @@
-/* Purpose: Validate password: minimum 8 characters */
+/* Purpose: Validate password with at least 1 upper case */
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -10,8 +10,8 @@ public class UserRegistration {
 	private void validateFirstName() {
 		Scanner scan = new Scanner(System.in);
 		System.out.println(" Enter first name: ");
-		String firstName = scan.next();			            //take first name from user
-		String regexFirstName = "^[A-Z]{1}[a-z]{2,}$" ;             //regex definition
+		String firstName = scan.next();				//take first name from user
+		String regexFirstName = "^[A-Z]{1}[a-z]{2,}$" ;         //regex definition
  		Pattern pattern = Pattern.compile(regexFirstName);    
 		Matcher first_nameMatcher = pattern.matcher(firstName);
 		System.out.println(first_nameMatcher.matches());
@@ -22,8 +22,8 @@ public class UserRegistration {
 	private void validateLastName() {
 		Scanner scan = new Scanner(System.in);
 		System.out.println(" Enter last name: ");
-		String lastName = scan.next();				   //take first name from user
-		String regexLastName = "^[A-Z]{1}[a-z]{2,}$";		   //regex definition
+		String lastName = scan.next();				 //take first name from user
+		String regexLastName = "^[A-Z]{1}[a-z]{2,}$";		 //regex definition
 		Pattern pattern = Pattern.compile(regexLastName);
 		Matcher last_nameMatcher = pattern.matcher(lastName);
 		System.out.println(last_nameMatcher.matches());
@@ -34,7 +34,7 @@ public class UserRegistration {
 	private void validateEmail() {
 		Scanner scan = new Scanner(System.in);
 		System.out.println("Enter email: ");
-		String email = scan.next();				   //take email from user
+		String email = scan.next();
 		String regexEmail = "^[A-Za-z0-9]+([-\\.\\+\\_]{1}[0-9A-Za-z]+)*@[A-Za-z0-9]+.[a-zA-Z]{2,4}([\\.\\,]{1}[a-z]{2,3}){0,1}$";
 		Pattern pattern = Pattern.compile(regexEmail);
 		Matcher emailMatcher = pattern.matcher(email);
@@ -46,11 +46,19 @@ public class UserRegistration {
 	private void validatePassword() {
 		Scanner scan = new Scanner(System.in);
 		System.out.println("Enter password with min 8 characters: ");
-		String password = scan.next();				 //take password from user
-		String regexPassword = "^.{8,}";			 //regex definition
-		Pattern pattern = Pattern.compile(regexPassword);
-		Matcher passwordMatcher = pattern.matcher(password);
-		System.out.println(passwordMatcher.matches());
+		String password = scan.next();
+		String regexPassword1 = "^.{8,}";			//regex pattern for min 8 characters
+		Pattern pattern1 = Pattern.compile(regexPassword1);
+		Matcher passwordMatcher1 = pattern1.matcher(password);
+		String regexPassword2 = ".*[A-Z]{1,}.*";	        //regex pattern for min 1 uppercase
+		Pattern pattern2 = Pattern.compile(regexPassword2);
+		Matcher passwordMatcher2 = pattern2.matcher(password);
+		if (passwordMatcher1.matches() && passwordMatcher2.matches() == true) 
+			System.out.println("true");	
+		else 
+			System.out.println("false");
+		
+
 	}
 	public static void main( String[] args ) {
 		UserRegistration registration = new UserRegistration();
@@ -58,7 +66,8 @@ public class UserRegistration {
 		registration.validateLastName();
 		registration.validateEmail();
 		registration.validatePassword();
-	}
-}
 
+	}
+
+}
 
